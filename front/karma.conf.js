8bece3928ca658c9dfe619800bb2d30784e0df7c
9109898N <alioune.beye@sncf.fr>
2024-07-17 16:23:59 +0200

    wip

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -1,41 +1,35 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
+    basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage-istanbul-reporter'), // Add this
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        // Jasmine options here
+      },
+      clearContext: false // Leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    jasmineHtmlReporter: {
+      suppressAll: true // Removes the duplicated traces
+    },
+    coverageIstanbulReporter: { // Updated to use coverage-istanbul
       dir: require('path').join(__dirname, './coverage/bobapp'),
       reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+    browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true,
-    captureTimeout: 60000,
-    browserDisconnectTolerance: 3,
-    browserDisconnectTimeout: 60000,
-    browserNoActivityTimeout: 60000
+    restartOnFileChange: true
   });
 };
